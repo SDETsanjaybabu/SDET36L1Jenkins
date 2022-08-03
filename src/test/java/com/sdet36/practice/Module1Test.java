839697f1bacd296bea667fdce2a86401bccc20da
mohangowdak <103551008+mohangowdak@users.noreply.github.com>
2022-08-03 12:41:46 +0530

    first commit

---
+++ src/test/java/com/sdet36/practice/Module1Test.java
@@ -0,0 +1,14 @@
+package com.sdet36.practice;
+
+import org.testng.annotations.Test;
+
+public class Module1Test {
+	
+	@Test
+	public void tc1Test() {
+		System.out.println("step1");
+		System.out.println("step2");
+		System.out.println("step3");
+	}
+
+}
